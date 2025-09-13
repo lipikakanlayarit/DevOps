5f286c9f32535031e0d83b42057fe6f2b5fb0dd4
woonjung15 <pimpisachumrosvit@gmail.com>
2025-09-13 17:35:20 +0700

    add test

---
+++ Backend/src/test/java/com/example/devops/web/AuthControllerTest.java
@@ -0,0 +1,35 @@
+package com.example.devops.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper; // ใช้แปลง object -> JSON
+
+    @Test
+    void shouldReturnTokenWhenLoginSuccess() throws Exception {
+        var req = new LoginReq("admin", "password123");
+
+        mockMvc.perform(post("/api/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(req)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.token").value(org.hamcrest.Matchers.containsString("fake-jwt-")));
+
+    }
+}
